b1be7abd3c33472542aa43cfeab9f720140eeceb
Dennis Luken <dennis@lukenconsulting.com>
2022-03-19 14:47:28 -0400

    env

--- projectgreen/environments/environment.prod.ts
+++ projectgreen/environments/environment.prod.ts
@@ -1,13 +1,16 @@
-const protocol = 'http://';
-const host = 'localhost:3000/';
+
+const protocol = 'https://';
+const host = 'www.njpotshop.com:3000/';
 const apiPath = 'api/v1/';
 
 export const environment = {
   production: true,
+  protocol: 'https://',
+  host: 'www.njpotshop.com:3000/',
+  apiPath: 'api/v1/',
   apiUrl: `${protocol}${host}${apiPath}`,
   imageUrl: `${protocol}${host}`,// 'http://localhost:3000/api/v1/',
 };
-
 
 
 
